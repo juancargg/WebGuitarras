cbc4e783df5aff62a47e4a9f4a5ca3c70c1bc2ae
juancargg <juancarggIT@gmail.com>
2024-09-09 20:03:27 +0200

    Mejorado el componente de guitar para que la cantidad vaya subiendo

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,9 @@ function App() {
     function addToCart(item) {
         const itemExists = cart.findIndex(guitar => guitar.id === item.id)
         if(itemExists >=0){
-
+            const updatedCart = [...cart]
+            updatedCart[itemExists].quantity++
+            setCart(updatedCart)
         }else{
             item.quantity = 1
             setCart([...cart, item])
