c51d891a0bf48ef739f3310b28b4f5d336acc047
juancargg <juancarggIT@gmail.com>
2024-09-09 19:26:37 +0200

    Terminado el componente de guitarra

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { useState, useEffect } from "react"
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
+import { db } from "./data/db"
 
 function App() {
-  
+
+    //State
+    const [data, setData] = useState(db)
+    const [cart, setCart] = useState([])
+ 
+    function addToCart(item) {
+        const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+        if(itemExists >=0){
+
+        }else{
+            item.quantity = 1
+            setCart([...cart, item])
+        }
+
+        
+    }
+
   return (
     <>
         <Header />
@@ -11,14 +29,22 @@ function App() {
             <h2 className="text-center">Nuestra Colección</h2>
 
             <div className="row mt-5">
-                <Guitar />
+                {data.map((guitar) => (
+                    <Guitar 
+                        key={guitar.id}
+                        guitar={guitar}
+                        setCart={setCart}
+                        addToCart={addToCart}
+
+                    />                
+                ))}
             </div>
         </main>
 
 
         <footer className="bg-dark mt-5 py-5">
             <div className="container-xl">
-                <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+                <p className="text-white text-center fs-4 mt-4 m-md-0">Guitarra Web - Todos los derechos Reservados</p>
             </div>
         </footer>
       
